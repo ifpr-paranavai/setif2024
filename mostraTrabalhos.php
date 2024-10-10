c4d327594e5e451e6c2dd4967cb49caa6cdb0ae3
Felipe Soares de Oliveira <felipesoaresdeoliveira13@gmail.com>
2024-10-10 18:06:56 -0300

    Atualização pagina Mostra trabalhos

---
+++ mostraTrabalhos.php
@@ -0,0 +1,29 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/setif/init.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <title>Mostra trabalhos</title>
+    <?php include_once LIB_INCLUDES_2024 . DS . 'metadados-novo.php' ?>
+</head>
+
+<body>
+    <?php include_once LIB_INCLUDES_2024 . DS . 'navbar-novo.php' ?>
+
+    <main class="container-fluid text-center mt-5 pt-4 "> 
+        <div class="row border-bottom mt-5 h-100 d-flex">
+            <div class="col-12">
+                <h2 class="py-3">
+                    Em Breve, mais informações sobre os trabalhos selecionados e ensalamento!
+                </h2>
+            </div>
+        </div>
+    </main>
+    <?php include_once LIB_INCLUDES_2024 . DS . 'rodape-novo.php' ?>
+    <?php include_once LIB_INCLUDES_2024 . DS . 'scripts.php' ?>
+</body>
+
+</html>
