a108e8cab24694bbbb30527563d3a0b406959de3
LuizHamamura <luizhamamura2627@outlook.com>
2024-11-04 22:24:14 -0300

    Atualização do estilo e ajustes nas normas de publicação e index.

--- index.php
+++ index.php
@@ -24,15 +24,40 @@ $dados = $controller->getDadosPaginaInstitucional(2024);
 
 
     <section class="seção-principal p-5 text-center">
-        <div class="d-flex flex-column align-items-center">
-            <img src="imagens/Logo_setif.png" alt="Logo SETIF" class="logo_setif">
-            <div class="botoes d-flex flex-column flex-md-row justify-content-center mt-3">
-                <button class="inscricao  me-md-3 mb-2 mb-md-0"
-                    onclick="window.location.href='https://www.even3.com.br/setif-2024/'">Inscreva-se</button>
-                <button class="saiba-mais" onclick="window.location.href='#'">Saiba mais</button>
+    <div class="d-flex flex-column align-items-center">
+        <img src="imagens/Logo_setif.png" alt="Logo SETIF" class="logo_setif">
+        <div class="botoes d-flex flex-column flex-md-row justify-content-center mt-3">
+            <button class="inscricao me-md-3 mb-2 mb-md-0" onclick="window.location.href='https://www.even3.com.br/setif-2024/'">Inscreva-se</button>
+            <button type="button" class="saiba-mais" data-bs-toggle="modal" data-bs-target="#eventoModal">
+                Saiba mais
+            </button>
+        </div>
+    </div>
+</section>
+
+<div class="modal fade" id="eventoModal" tabindex="-1" aria-labelledby="eventoModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen-below-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="eventoModalLabel">Informações do Evento</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+            <div class="modal-body">
+                <p>A Semana de Tecnologia da Informação 
+                    do IFPR Campus Paranavaí (SETIF 2024) ocorrerá 
+                    de 3 a 7 de dezembro, oferecendo minicursos, 
+                    palestras, competições e uma mostra de trabalhos
+                     acadêmicos. O evento é gratuito e acontecerá 
+                     presencialmente e online, reunindo estudantes e 
+                     profissionais para discussões e práticas na área de TI.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
             </div>
         </div>
-    </section>
+    </div>
+</div>
+
 
 
     <section class="parallax-programacao h-100">
--- normasPublicacao.php
+++ normasPublicacao.php
@@ -33,18 +33,9 @@ $normas = $controller->getNormasPublicacao('2024');
                 <h2><i class="bi bi-calendar-check"></i> Datas Importantes</h2>
 
                 <div class="mt-4">
-                    <h4>Data limite para a cadastro de trabalhos:</h4>
-                    <div class="alert alert-secondary text-lowercase text-decoration-line-through">
+                    <h4>Data limite para a submissão de trabalhos:</h4>
+                    <div class="alert alert-danger text-lowercase">
                         <?= Funcoes::getDataPorExtenso($normas->getDataLimite()) ?>
-                    </div>
-                    <div class="alert alert-danger text-lowercase">
-                        14 de outubro de 2024
-                    </div>
-                </div>
-                <div class="mt-4">
-                    <h4>Data limite para a <em>upload</em> dos arquivos:</h4>
-                    <div class="alert alert-danger text-lowercase">
-                        17 de outubro de 2024
                     </div>
                 </div>
 
@@ -80,33 +71,36 @@ $normas = $controller->getNormasPublicacao('2024');
 
             <div class="col-12 col-md-6 p-4 text-secondary normas-submissao">
                 <h2>Submissão</h2>
-                <p class="fs-5">No sistema, cada submissão deve ser feita em duas etapas:</p>
+                <p class="fs-5">A submissão deve ser feita em duas etapas:</p>
                 <ol>
                     <li>Clique em "Enviar Submissão" para cadastrar o artigo e autores (salve o ID e senha).</li>
                     <li>Clique em "Enviar Arquivo" para fazer upload do artigo em PDF.</li>
                 </ol>
-                <p class="fs-5">Sistema de submissão: <a href="https://tecnoif.com.br/sistemas/openconf/openconf.php"
-                        target="_blank" class="btn btn-outline-success">Clique aqui</a></p>
+                <p class="fs-5">Sistema de submissão: 
+                    <a href="https://tecnoif.com.br/sistemas/openconf/openconf.php" target="_blank" class="btn btn-outline-success">Clique aqui</a>
+                </p>
 
                 <h2 class="mt-4">Artigos completos</h2>
-                <p class="fs-5">Os artigos completos devem ter no máximo 12 páginas, seguindo o modelo da SBC:</p>
+                <p class="fs-5">Devem ter no máximo 12 páginas, seguindo o modelo da SBC:</p>
                 <ul>
-                    <li>LaTeX: <a href="https://www.overleaf.com/latex/templates/sbc-conferences-template/blbxwjwzdngr"
-                            target="_blank" class="btn btn-outline-success">Ver template no Overleaf</a></li>
-                    <li class="mt-2">Modelo SBC .doc: <a
-                            href="https://drive.google.com/file/d/1H_29Hg5N534qOTwKOwnVMRWEDHZgIFMv/view"
-                            class="btn btn-outline-success">Baixar template .doc</a></li>
+                    <li>LaTeX: 
+                        <a href="https://www.overleaf.com/latex/templates/sbc-conferences-template/blbxwjwzdngr" target="_blank" class="btn btn-outline-success">Ver template no Overleaf</a>
+                    </li>
+                    <li class="mt-2">Modelo SBC .doc: 
+                        <a href="https://drive.google.com/file/d/1H_29Hg5N534qOTwKOwnVMRWEDHZgIFMv/view" class="btn btn-outline-success">Baixar template .doc</a>
+                    </li>
                 </ul>
 
                 <h2 class="mt-4">Resumos</h2>
-                <p class="fs-5">Os resumos devem ter 3 páginas e estar formatados de acordo com o modelo SBC:</p>
+                <p class="fs-5">Devem ter 3 páginas e estar formatados de acordo com o modelo SBC:</p>
                 <ul>
-                    <li>LaTeX: <a href="https://www.overleaf.com/latex/templates/sbc-conferences-template/blbxwjwzdngr"
-                            target="_blank" class="btn btn-outline-success">Ver template no Overleaf</a></li>
+                    <li>LaTeX: 
+                        <a href="https://www.overleaf.com/latex/templates/sbc-conferences-template/blbxwjwzdngr" target="_blank" class="btn btn-outline-success">Ver template no Overleaf</a>
+                    </li>
                     <li class="text-danger">Não incluir abstract no modelo LaTeX</li>
-                    <li class="mt-2">Modelo SBC .doc: <a
-                            href="https://drive.google.com/file/d/1H_29Hg5N534qOTwKOwnVMRWEDHZgIFMv/view"
-                            class="btn btn-outline-success">Baixar template .doc</a></li>
+                    <li class="mt-2">Modelo SBC .doc: 
+                        <a href="https://drive.google.com/file/d/1H_29Hg5N534qOTwKOwnVMRWEDHZgIFMv/view" class="btn btn-outline-success">Baixar template .doc</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -114,11 +108,9 @@ $normas = $controller->getNormasPublicacao('2024');
 
     <section class="container text-center normas-apresentacao my-5">
         <h2 class="py-3">Apresentação</h2>
-        <p class="fs-4">Os trabalhos aceitos deverão ser apresentados no SETIF 2024 em apresentação oral presencial.
-            Detalhes sobre a agenda serão divulgados após a revisão dos trabalhos.</p>
+        <p class="fs-4">Os trabalhos aceitos deverão ser apresentados no SETIF 2024 em apresentação oral presencial. Detalhes sobre a agenda serão divulgados após a revisão dos trabalhos.</p>
         <h2 class="py-3"><i class="bi bi-clock"></i> Tempo de Apresentação</h2>
-        <p><?= $normas->getTempoApresentacaoResumo() ?> minutos para resumos,
-            <?= $normas->getTempoApresentacaoArtigo() ?> minutos para artigos completos.</p>
+        <p><?= $normas->getTempoApresentacaoResumo() ?> minutos para resumos, <?= $normas->getTempoApresentacaoArtigo() ?> minutos para artigos completos.</p>
     </section>
 
     <section class="normas-topicos text-white py-5">
